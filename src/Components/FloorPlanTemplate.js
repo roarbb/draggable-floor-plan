a9829b297d265feb08845ec1fffd0da4fa523c1e
Matej Sajgal <roarbb@gmail.com>
2019-03-21 22:06:43 +1100

    add table functional component, updated grid layout

--- src/Components/FloorPlanTemplate.js
+++ src/Components/FloorPlanTemplate.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './FloorPlanTemplate.css';
 
-class FloorPlanTemplate extends Component{
-  render(){
+function Table(props) {
+  const orientation = props.orientation || 'horizontal';
+
+  return <div className={`table ${orientation}`} />;
+}
+
+class FloorPlanTemplate extends Component {
+  render() {
     return (
-      <div className="floor">
-        <div className="hole"></div>
+      <div className="grid">
+        <div className="floor">
+          <div className="hole" />
+          <Table id="table-1" orientation="vertical" />
+        </div>
       </div>
     )
   }
